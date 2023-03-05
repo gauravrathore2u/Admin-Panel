736bd71452d168e313a241b75fd11e8932030611
Gaurav Rathore <gauravrathore2u@gmail.com>
2023-03-05 18:45:37 +0530

    uptoNavBar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,22 @@
 import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar'
 import './home.scss';
+import Widget from '../../components/widget/Widget';
 
 const Home = () => {
   return (
     <div className='home'>
         <Sidebar/>
-        <div className="homeContainer">Container</div>
+        <div className="homeContainer">
+          <Navbar/>
+          <div className="widgets">
+            <Widget type='user'/>
+            <Widget type='order'/>
+            <Widget type='earning'/>
+            <Widget type='balance'/>
+          </div>
+          Home Container
+        </div>
     </div>
   )
 }
